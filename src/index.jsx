9f28ac84ac84f41a87d19643e5516033448feee9
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2021-12-22 21:59:14 -0500

    Changing index to a jsx file and delete web vitals

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.scss";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  // eslint-disable-next-line no-undef
+  document.getElementById("root")
+);
